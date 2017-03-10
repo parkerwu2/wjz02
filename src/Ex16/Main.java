ab5ff99cd23fc9d729422ef3d88b631de85a5d87
wujingzhi <wujingzhi@superjia.com>
2017-03-10 18:40:48 +0800

    up

---
+++ src/Ex16/Main.java
@@ -0,0 +1,69 @@
+package Ex16;
+
+import java.io.*;
+
+class Step {
+    public volatile int value = 1;
+    synchronized public int get() {
+        return value % 3;
+    }
+    synchronized public void add(int n) {
+        value += n;
+    }
+}
+class A extends Thread {
+    private Step step;
+    public A(Step s) {
+        step = s;
+    }
+    @Override
+    public void run() {
+        synchronized (step) {
+            for (int i = 1; i <= 52; i+= 2) {
+                while (step.get() == 0) {
+                    try {
+                        step.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.print(i + String.valueOf(i + 1));
+                step.add(2);
+                step.notifyAll();
+            }
+        }
+    }
+}
+class B extends Thread {
+    private Step step;
+    public B(Step s) {
+        step = s;
+    }
+    String s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    @Override
+    public void run() {
+        synchronized (step) {
+            for (int i = 0; i < 26; i++) {
+                while (step.get() != 0) {
+                    try {
+                        step.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.print(s.charAt(i));
+                step.add(1);
+                step.notifyAll();
+            }
+        }
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Step s = new Step();
+        new A(s).start();
+        new B(s).start();
+    }
+}
--- src/Main2.java
+++ src/Main2.java
@@ -1,11 +1,33 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by Administrator on 2017/2/27.
  */
 public class Main2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        List<Integer> l = new ArrayList<>(20);
+        //int nThreads = 30;
+        //ThreadPoolExecutor tr = new  ThreadPoolExecutor(nThreads, nThreads, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+        BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>(10);
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(10,40,0L,TimeUnit.SECONDS, workQueue);
+        final AtomicInteger index = new AtomicInteger(0);
+        //test2();
+        for (int i = 0 ; i < 1000 ; i++ ) {
+            System.out.println("cycle"+i);
+                threadPoolExecutor.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        throw new NullPointerException("测试异常" + index.getAndIncrement());
+                    }
+                });
 
+            System.out.println("out:" + i);
+        }
+        System.out.println("over");
     }
 }
